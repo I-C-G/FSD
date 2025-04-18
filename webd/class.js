fae8f518f7d3e3bd56fca3b8602d55499e0cb5f2
I-C-G <ishanchandra.ic@gmail.com>
2025-04-18 13:03:53 +0530

    OOPS

--- webd/class.js
+++ webd/class.js
@@ -26,30 +26,84 @@
 //There can only be 1 constructor in a class.
 //It is convention to start class name with a capital letter
 
-class Person{
-    constructor(firstname, lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
-}
-let person1=new Person("Ishan", "Chandra");
-console.log("Hi",person1.firstname);
-// METHODS:
-// Methods are functions that belong to an object.
+// class Person{
+//     constructor(firstname, lastname) {
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//     }
+// }
+// let person1=new Person("Ishan", "Chandra");
+// console.log("Hi",person1.firstname);
+// // METHODS:
+// // Methods are functions that belong to an object.
 
-class Person{
-    constructor(firstname, lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
-    greet() {
-        console.log("Hello", this.firstname, this.lastname);
-    }
-    compliment(name,object){
-        return "That's a wonderful " + object + " " + name + "!";
-    }
-}
-let person1=new Person("Ishan", "Grover");
-let compliment=person1.compliment("Ishan", "car");
-console.log(compliment);
-person1.greet();
+// class Person{
+//     constructor(firstname, lastname) {
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//     }
+//     greet() {
+//         console.log("Hello", this.firstname, this.lastname);
+//     }
+//     compliment(name,object){
+//         return "That's a wonderful " + object + " " + name + "!";
+//     }
+// }
+// let person1=new Person("Ishan", "Grover");
+// let compliment=person1.compliment("Ishan", "car");
+// console.log(compliment);
+// person1.greet();
+
+// class Person{
+//     #firstname;
+//     #lastname;
+//     constructor(firstname,lastname){
+//            this.#firstname=firstname;
+       
+//         this.#lastname=lastname;
+
+//     }
+//     get firstname(){
+//         return this.#firstname;
+//     }
+//     set firstname(firstname){
+// this.#firstname=firstname;
+//     }
+//     get lastname(){
+//         return this.#lastname;
+//     }
+//     set lastname(lastname){
+// this.#lastname=lastname;
+//     }
+// }
+// let q=new Person("Harry","hat");
+// console.log(q.firstname);
+// class Vehicle {
+//     constructor(make, model, year, color) {
+//         this.make = make;
+//         this.model = model;
+//         this.year = year;
+//         this.color = color;
+//     }
+//     start() {
+//         console.log("Starting vehicle...");
+//     }
+//     stop() {
+//         console.log("Stopping vehicle...");
+//     }
+// }
+// class Motorcycle extends Vehicle {
+//     constructor(make, model, year, color) {
+//         super(make, model, year, color);
+//         this.type = "Motorcycle";
+//     }
+//     start() {
+//         console.log("Starting motorcycle...");
+//     }
+//     stop() {
+//         console.log("Stopping motorcycle...");
+//     }
+// }
+
+// let motorcycle1 = new Motorcycle("Harley Davidson", "Street 750", 2020, "Black");
+// console.log(motorcycle1.color);
